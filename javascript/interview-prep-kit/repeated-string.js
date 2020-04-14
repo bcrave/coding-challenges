285a839b0fd61630b0d3d98d910a4ce7f28fe996
bcrave <brennycode@gmail.com>
2020-04-14 14:00:42 +0200

    repeated-string attempt 2

--- javascript/interview-prep-kit/repeated-string.js
+++ javascript/interview-prep-kit/repeated-string.js
@@ -1,26 +1,36 @@
 // Print a single integer denoting the number of letter a's in the first  letters of the infinite string created by repeating  infinitely many times.
 
 // first attempt.  It technically works but isn't scalable.
-const repeatedString = (s, n) => {
-  let string = [];
-  let count = 0;
+// const repeatedString = (s, n) => {
+//   let string = [];
+//   let count = 0;
 
-  while (string.length < n) {
-    for (let i = 0; i < s.length; i++) {
-      if (string.length === n) {
-        break;
-      } else {
-        string.push(s[i]);
-      }
-    }
-  }
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] === "a") {
-      count++;
-    }
-  }
-  return count;
+//   while (string.length < n) {
+//     for (let i = 0; i < s.length; i++) {
+//       if (string.length === n) {
+//         break;
+//       } else {
+//         string.push(s[i]);
+//       }
+//     }
+//   }
+//   for (let i = 0; i < string.length; i++) {
+//     if (string[i] === "a") {
+//       count++;
+//     }
+//   }
+//   return count;
+// };
+
+// second attempt.  Learned solution in python and refactored into javascript.
+const repeatedString = (s, n) => {
+  const aCount = (str) => str.split("").filter((count) => count == "a").length;
+
+  const num = Math.floor(n / s.length);
+  const mod = n % s.length;
+
+  return aCount(s) * num + aCount(s.substr(0, mod));
 };
 
 console.log(repeatedString("aba", 10));
-// console.log(repeatedString("a", 1000000000000));
+console.log(repeatedString("a", 1000000000000));
